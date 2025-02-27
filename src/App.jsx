b31b7cbe9f5d1c74496502810489b29188038a25
wbharath <wbharath51@gmail.com>
2025-02-27 19:51:35 -0400

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+import Navbar from './components/Navbar'
+import CartContainer from './components/CartContainer'
+import { useDispatch, useSelector } from 'react-redux'
+import { calculateTotals } from './features/cart/cartSlice'
+import Modal from './components/Modal'
+
+function App() {
+  const { cartItems } = useSelector((store) => store.cart)
+  const { isOpen } = useSelector((store) => store.modal)
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(calculateTotals())
+  }, [cartItems])
+
+  return (
+    <main>
+      {isOpen && <Modal />}
+
+      <Navbar />
+      <CartContainer />
+    </main>
+  )
+}
+
+export default App
