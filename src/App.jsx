46c361ed150428572593ab98794cbd1874a5f559
wbharath <wbharath51@gmail.com>
2025-02-28 15:22:24 -0400

    Implemented extra reducers using builder callback notation

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ import './index.css'
 import Navbar from './components/Navbar'
 import CartContainer from './components/CartContainer'
 import { useDispatch, useSelector } from 'react-redux'
-import { calculateTotals } from './features/cart/cartSlice'
+import { calculateTotals, getCartItems } from './features/cart/cartSlice'
 import Modal from './components/Modal'
 
 function App() {
-  const { cartItems } = useSelector((store) => store.cart)
+  const { cartItems, isLoading } = useSelector((store) => store.cart)
   const { isOpen } = useSelector((store) => store.modal)
 
   console.log(cartItems)
@@ -18,6 +18,17 @@ function App() {
   useEffect(() => {
     dispatch(calculateTotals())
   }, [cartItems])
+
+
+  useEffect(()=>{
+    dispatch(getCartItems('random'))
+  },[])
+
+  if(isLoading){
+    return <div className="loading">
+    <h1>...loading</h1>
+    </div>
+  }
 
   return (
     <main>
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,51 +1,81 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
 
-import cartItems from '../../cartItems'
-
+const url = 'https://www.course-api.com/react-useReducer-cart-project'
 
 const initialState = {
-  cartItems: cartItems,
+  cartItems: [],
   amount: 4,
   total: 0,
-  isLoading: true,
-};
+  isLoading: true
+}
 
-
+export const getCartItems = createAsyncThunk(
+  'cart/getCartItems',
+  async (name, thunkAPI) => {
+    try {
+      // console.log(name);
+      // console.log(thunkAPI);
+      // console.log(thunkAPI.getState());
+      // thunkAPI.dispatch(openModal());
+      const resp = await axios(url)
+      console.log('Response:', resp.data)
+      return resp.data
+    } catch (error) {
+      return thunkAPI.rejectWithValue('something went wrong')
+    }
+  }
+)
 
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
     clearCart: (state) => {
-      state.cartItems = [];
+      state.cartItems = []
     },
     removeItem: (state, action) => {
-      const itemId = action.payload;
-      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+      const itemId = action.payload
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId)
     },
     increase: (state, { payload }) => {
-      const cartItem = state.cartItems.find((item) => item.id === payload.id);
-      cartItem.amount = cartItem.amount + 1;
+      const cartItem = state.cartItems.find((item) => item.id === payload.id)
+      cartItem.amount = cartItem.amount + 1
     },
     decrease: (state, { payload }) => {
-      const cartItem = state.cartItems.find((item) => item.id === payload.id);
-      cartItem.amount = cartItem.amount - 1;
+      const cartItem = state.cartItems.find((item) => item.id === payload.id)
+      cartItem.amount = cartItem.amount - 1
     },
     calculateTotals: (state) => {
-      let amount = 0;
-      let total = 0;
+      let amount = 0
+      let total = 0
       state.cartItems.forEach((item) => {
-        amount += item.amount;
-        total += item.amount * item.price;
-      });
-      state.amount = amount;
-      state.total = total;
-    },
+        amount += item.amount
+        total += item.amount * item.price
+      })
+      state.amount = amount
+      state.total = total
+    }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getCartItems.pending, (state) => {
+        state.isLoading = true
+      })
+      .addCase(getCartItems.fulfilled, (state, action) => {
+        // console.log(action);
+        state.isLoading = false
+        state.cartItems = action.payload
+      })
+      .addCase(getCartItems.rejected, (state, action) => {
+        console.log(action)
+        state.isLoading = false
+      })
   }
-});
+})
 
 // console.log(cartSlice);
 export const { clearCart, removeItem, increase, decrease, calculateTotals } =
-  cartSlice.actions;
+  cartSlice.actions
 
-export default cartSlice.reducer;
+export default cartSlice.reducer
